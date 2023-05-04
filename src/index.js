ffc75a353e9d0a87fd89be988c852ef599d03fc3
BezzB <118984781+BezzB@users.noreply.github.com>
2023-05-04 16:31:40 +0300

    Update index html

--- src/index.js
+++ src/index.js
@@ -1,90 +1,104 @@
 import './css/mainCss.css';
 
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-const searchResults = document.getElementById('search-results');
+// const searchInput = document.getElementById('search-input');
+// const searchButton = document.getElementById('search-button');
+// const searchResults = document.getElementById('search-results');
 
-searchButton.addEventListener('click', () => {
-  const query = searchInput.value;
-  const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+// searchButton.addEventListener('click', () => {
+//   const query = searchInput.value;
+//   const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const { meals } = data;
-      if (meals) {
-        searchResults.innerHTML = meals.map((meal) => `
-          <div class="meal">
-            <h3>${meal.strMeal}</h3>
-            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-            <p>${meal.strInstructions}</p>
-          </div>
-        `).join('');
-      } else {
-        searchResults.innerHTML = '<p>No meals found, try another search query.</p>';
-      }
-    })
-    .catch((error) => console.error(error));
-});
+//   fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const { meals } = data;
+//       if (meals) {
+//         searchResults.innerHTML = meals.map((meal) => `
+//           <div class="meal">
+//             <h3>${meal.strMeal}</h3>
+//             <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+//             <p>${meal.strInstructions}</p>
+//           </div>
+//         `).join('');
+//       } else {
+//         searchResults.innerHTML = '<p>No meals found, try another search query.</p>';
+//       }
+//     })
+//     .catch((error) => console.error(error));
+// });
 
-// Get the section element from the HTML
-const section = document.getElementById('cards');
+const mealsEndpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s';
+const mealDetailsEndpoint = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+const cardsContainer = document.getElementById('cards');
 
-// Make a request to the API
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
+fetch(mealsEndpoint)
   .then((response) => response.json())
   .then((data) => {
-    // Get the first six meals from the API
-    const meals = data.meals.slice(0, 6);
+    const meals = data.meals.slice(0, 6); // get the first 6 meals from the data
 
-    // Create a card for each meal
     meals.forEach((meal) => {
-      // Create the elements for the card
       const card = document.createElement('div');
-      card.classList.add('card');
+      card.className = 'card';
 
       const imageContainer = document.createElement('div');
-      imageContainer.classList.add('image-container');
+      imageContainer.className = 'image-container';
 
       const image = document.createElement('img');
       image.src = meal.strMealThumb;
       image.alt = meal.strMeal;
 
+      imageContainer.appendChild(image);
+
       const title = document.createElement('div');
-      title.classList.add('title');
+      title.className = 'title';
 
       const heading = document.createElement('h2');
       heading.textContent = meal.strMeal;
 
-      const likeButton = document.createElement('button');
-      likeButton.classList.add('like');
-      likeButton.textContent = '5 Likes';
+      const likeBtn = document.createElement('button');
+      likeBtn.className = 'like';
+      likeBtn.textContent = 'Likes';
+
+      const likeCounter = document.createElement('span');
+      likeCounter.className = 'like-counter';
+      likeCounter.textContent = 'Loading...'; // show a loading message while fetching the likes counter
+
+      title.appendChild(heading);
+      title.appendChild(likeBtn);
+      title.appendChild(likeCounter);
 
       const comments = document.createElement('div');
-      comments.classList.add('comments');
+      comments.className = 'comments';
 
-      const commentButton = document.createElement('button');
-      commentButton.classList.add('comment');
-      commentButton.textContent = 'Comments';
+      const commentBtn = document.createElement('button');
+      commentBtn.className = 'comment';
+      commentBtn.textContent = 'Comments';
 
-      const reserveButton = document.createElement('button');
-      reserveButton.classList.add('reserve');
-      reserveButton.textContent = 'Reservations';
+      const reserveBtn = document.createElement('button');
+      reserveBtn.className = 'reserve';
+      reserveBtn.textContent = 'Reservations';
 
-      // Append the elements to the card
-      imageContainer.appendChild(image);
-      title.appendChild(heading);
-      title.appendChild(likeButton);
-      comments.appendChild(commentButton);
-      comments.appendChild(reserveButton);
+      comments.appendChild(commentBtn);
+      comments.appendChild(reserveBtn);
+
       card.appendChild(imageContainer);
       card.appendChild(title);
       card.appendChild(comments);
 
-      // Append the card to the section
-      section.appendChild(card);
+      cardsContainer.appendChild(card);
+
+      // make a request to get the meal details, including the likes counter
+      fetch(`${mealDetailsEndpoint}${meal.idMeal}`)
+        .then((response) => response.json())
+        .then((data) => {
+          likeCounter.textContent = data.meals[0].strInstructions.match(/\d+/)[0]; // extract the number of likes from the instructions text
+        })
+        .catch((error) => {
+          console.error('Error fetching meal details:', error);
+          likeCounter.textContent = 'N/A'; // show "N/A" if there was an error fetching the likes counter
+        });
     });
   })
   .catch((error) => {
-    console.error(error);
+    console.error('Error fetching data:', error);
   });
