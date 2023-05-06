141c07cf6323bc1ff8b3d3f60fbc565406f05668
BranBayou <berhan.baye@gmail.com>
2023-05-06 09:35:04 +0300

    Add comment and count commetn

--- src/index.js
+++ src/index.js
@@ -1,62 +1,101 @@
 import './css/mainCss.css';
 import './css/reservation.css';
+import { displayComments, postComment } from './modules/involvement.js';
 
-// Get the section element from the HTML
-const section = document.getElementById('cards');
+const mealsEndpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s';
+const likesEndpoint = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/MkVxBIukKyzNPTtQYW83/likes';
+const cardsContainer = document.getElementById('cards');
 
-// Make a request to the API
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
+async function updateLikes(mealId) {
+  try {
+    await fetch(likesEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({ item_id: mealId }),
+    });
+  } catch (error) {
+    throw ('Error updating likes:', error);
+  }
+}
+async function getLikes(mealId, spanel) {
+  try {
+    const response = await fetch(likesEndpoint);
+    const data = await response.json();
+    Array.from(data).forEach((el) => {
+      if (el.item_id === mealId) {
+        const countOfLikes = el.likes;
+        if (countOfLikes === 1) {
+          spanel.innerHTML = `${countOfLikes} like`;
+        } else if (countOfLikes === 0) {
+          spanel.innerHTML = '0 likes';
+        } else {
+          spanel.innerHTML = `${countOfLikes} likes`;
+        }
+      }
+    });
+    // console.log(data);
+  } catch (error) {
+    throw ('Error updating likes:', error);
+  }
+}
+
+fetch(mealsEndpoint)
   .then((response) => response.json())
   .then((data) => {
-    // Get the first six meals from the API
-    const meals = data.meals.slice(0, 6);
+    const meals = data.meals.slice(0, 6); // get the first 6 meals from the data
 
-    // Create a card for each meal
     meals.forEach((meal) => {
-      // Create the elements for the card
       const card = document.createElement('div');
-      card.classList.add('card');
+      card.className = 'card';
 
       const imageContainer = document.createElement('div');
-      imageContainer.classList.add('image-container');
+      imageContainer.className = 'image-container';
 
       const image = document.createElement('img');
       image.src = meal.strMealThumb;
       image.alt = meal.strMeal;
 
+      imageContainer.appendChild(image);
+
       const title = document.createElement('div');
-      title.classList.add('title');
+      title.className = 'title';
 
       const heading = document.createElement('h2');
       heading.textContent = meal.strMeal;
 
-      const likeButton = document.createElement('button');
-      likeButton.classList.add('like');
-      likeButton.textContent = '5 Likes';
+      const likeBtn = document.createElement('button');
+      likeBtn.className = 'like';
+      likeBtn.innerHTML = '<i class="fa-solid fa-heart"></i>';
+
+      const likeCounter = document.createElement('span');
+      likeCounter.className = 'like-counter';
+      // likeCounter.textContent = 'Loading...';
+      title.appendChild(heading);
+      title.appendChild(likeBtn);
+      title.appendChild(likeCounter);
 
       const comments = document.createElement('div');
-      comments.classList.add('comments');
+      comments.className = 'comments';
 
-      const commentButton = document.createElement('button');
-      commentButton.classList.add('comment');
-      commentButton.textContent = 'Comments';
+      const commentBtn = document.createElement('button');
+      commentBtn.className = 'comment';
+      commentBtn.textContent = 'Comments';
 
-      const reserveButton = document.createElement('button');
-      reserveButton.classList.add('reserve');
-      reserveButton.textContent = 'Reservations';
+      const reserveBtn = document.createElement('button');
+      reserveBtn.className = 'reserve';
+      reserveBtn.textContent = 'Reservations';
 
-      // Append the elements to the card
-      imageContainer.appendChild(image);
-      title.appendChild(heading);
-      title.appendChild(likeButton);
-      comments.appendChild(commentButton);
-      comments.appendChild(reserveButton);
+      comments.appendChild(commentBtn);
+      comments.appendChild(reserveBtn);
+
       card.appendChild(imageContainer);
       card.appendChild(title);
       card.appendChild(comments);
 
       // Append the card to the section
-      section.appendChild(card);
+      cardsContainer.appendChild(card);
       // popup window
       const popupSection = document.createElement('section');
       popupSection.setAttribute('id', `popup-modal-${meal.idMeal}`);
@@ -69,42 +108,60 @@ fetch('https://www.themealdb.com/api/json/v1/1/search.php?s')
                                       <h2>${meal.strMeal}</h2>
                                       <div class="meal-info"></div>
                                     </div>
-                                    <div class="comment-list">
+                                    <div class="comment-list" id="display-${meal.idMeal}">
+                                      <h3>Comments(0)</h3>
+                                      <div class="comments-container" id="container-${meal.idMeal}">
                                       
+                                      </div>
                                     </div>
                                     <div class="bottom">
                                       <h3>Add comment</h3>
-                                      <form action="" class="form">
-                                        <input type="text" placeholder="your name">
-                                        <input class="txt-area" type="text-area" placeholder="your insights">
-                                        <button class="add-comment" type="button">Add comment</button>
+                                      <form class="form">
+                                        <input type="text" id='customer_name_${meal.idMeal}' placeholder="your name">
+                                        <input class="txt-area" id ='customer_msg_${meal.idMeal}' type="text-area" placeholder="your insights">
+                                        <button class="add-comment" type="submit">Add comment</button>
                                       </form>
                                     </div>
                                   </div>
                                   `;
-      card.appendChild(popupSection);
 
-      commentButton.addEventListener('click', (event) => {
+      document.body.appendChild(popupSection);
+      const form = popupSection.querySelector('form');
+      form.addEventListener('submit', (event) => {
         event.preventDefault();
-        document.getElementById(`popup-modal-${meal.idMeal}`).style.display = 'block';
+        const username = document.getElementById(`customer_name_${meal.idMeal}`).value;
+        const comment = document.getElementById(`customer_msg_${meal.idMeal}`).value;
+        postComment(meal.idMeal, username, comment).then(() => {
+          displayComments(meal);
+        });
+        form.reset();
       });
 
-      // Event listners for popup
-      // const conBtns = document.getElementsByClassName('comment');
-      // for (let i = 0; i < conBtns.length; i += 1) {
-      //   conBtns[i].addEventListener('click', (event) => {
-      //     event.preventDefault();
-      //     document.getElementById(`popup-modal-${meal.idMeal}`).style.display = 'block';
-      //   });
-      // }
-      const closeBtn = document.querySelectorAll('.close');
-      closeBtn.forEach((btn) => {
-        btn.addEventListener('click', () => {
-          document.querySelector('.modal').style.display = 'none';
-        });
+      commentBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.getElementById(`popup-modal-${meal.idMeal}`).style.display = 'block';
+        displayComments(meal);
+      });
+
+      // add click event listener to the like button
+      getLikes(meal.idMeal, likeCounter);
+
+      likeBtn.addEventListener('click', async () => {
+        // increment the like counter
+        likeCounter.textContent = parseInt(likeCounter.textContent, 10) + 1;
+        await updateLikes(meal.idMeal);
+        getLikes(meal.idMeal, likeCounter);
+      });
+    });
+    const closeBtn = document.querySelectorAll('.close');
+    closeBtn.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.modal');
+        // eslint-disable-next-line no-return-assign
+        modals.forEach((modal) => modal.style.display = 'none');
       });
     });
   })
   .catch((error) => {
-    error(error);
+    error('Error fetching data:', error);
   });
---
+++ src/modules/involvement.js
@@ -0,0 +1,43 @@
+import commentCounter from './commentCount.js';
+
+const appId = 'MkVxBIukKyzNPTtQYW83';
+const involvementUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+
+const postComment = async (mealId, name, comm) => {
+  const url = `${involvementUrl}apps/${appId}/comments`;
+  const data = { item_id: `section-${mealId}`, username: name, comment: comm };
+
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  return response.text();
+};
+
+const displayComments = async (meal) => {
+  const commentDisplay = document.getElementById(`display-${meal.idMeal}`);
+  const getComments = async (url) => {
+    const response = await fetch(url);
+    const commentArray = await response.json();
+    return commentArray;
+  };
+
+  const comments = await getComments(`${involvementUrl}apps/${appId}/comments?item_id=section-${meal.idMeal}`);
+
+  const commentsContainer = document.getElementById(`container-${meal.idMeal}`);
+  commentsContainer.innerHTML = '';
+
+  if (comments.length > 0) {
+    comments.forEach((element) => {
+      const comment = document.createElement('p');
+      comment.innerHTML = `${element.creation_date} ${element.username}: ${element.comment}`;
+      commentsContainer.appendChild(comment);
+    });
+  }
+  commentDisplay.firstElementChild.textContent = `Comments (${commentCounter(meal)})`;
+};
+export { postComment, displayComments };
